c342ab826a9db4d4b2e5b0a13592de4f376fbcac
simbarashe <simbarashechigwada@gmail.com>
2022-10-13 13:04:35 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,18 @@
-#include <stdio.h>
+#include <stdio.h>
 
-
-
-/**
- *
- *  * main - Entry pointA
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-int main(void)
-
+	/**
+	 * main - Entry pointA
+	*
+	* Return: Always 0 (Success)
+	**/
+	int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 
 }
+
+
+
 
 
