0c3e5fc9da626a9463b8d9c322c7cf2026ee9580
simbarashe <simbarashechigwada@gmail.com>
2022-10-24 03:56:43 -0700

    Task 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 /**
 * reset_to_98 - Resets to 98.
-* @n : n is the pointer that shld return value 98
+* n : n is the pointer that shld return value 98
 * Return: Always 0.
 */
 
-void reset_to_98(int *n)
+int main(void)
 
 {
+	int n;
 
-		*n = 98;
-
+	n = 402;
+	printf("n=%d\n", n);
+	reset_to_98(&n);
+	printf("n=%d\n", n);
+	return (0);
 }
+
 
