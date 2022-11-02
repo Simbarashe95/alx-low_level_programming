76da8038055a04018882beeb42bfa0f1d279be61
simbarashe <simbarashechigwada@gmail.com>
2022-11-02 09:54:46 -0700

    Task 100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+* long_1 - check the code for long string.
+* @s: number
+*
+* Return: Always 0.
+*/
+int long_1(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + long_1(s + 1));
+	}
+	return (0);
+}
+/**
+* compare -  compares each character of the string.
+* @s: number
+* @l: lenght
+*
+* Return: Always 0.
+*/
+int compare(char *s, int l)
+{
+	if (l <= 0)
+	{
+		return (1);
+	}
+	if (*s == *(s + (l - 1)))
+	{
+		return (compare(s + 1, l - 2));
+	}
+	else
+	{
+		return (0);
+	}
+}
+/**
+* is_palindrome - detects if a string is a palindrome.
+* @s: character
+*
+* Return: Always 0.
+*/
+int is_palindrome(char *s)
+{
+	int l;
+
+	l = long_1(s);
+	return (compare(s, l));
+}
