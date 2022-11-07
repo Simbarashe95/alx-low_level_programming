32251e16e33889297d82701cc3253f3f07149335
simbarashe <simbarashechigwada@gmail.com>
2022-11-07 00:59:39 -0800

    Task main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -2,32 +2,44 @@
 
 #define MAIN_H
 
-
-
 int _putchar(char c);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int _abs(int n);
 
 int _isupper(int c);
 
 int _isdigit(int c);
 
-int mul(int a, int b);
+int _strlen(char *s);
 
-void print_numbers(void);
+void _puts(char *s);
 
-void print_most_numbers(void);
+char *_strcpy(char *dest, char *src);
 
-void more_numbers(void);
+int _atoi(char *s);
 
-void print_line(int n);
+char *_strcat(char *dest, char *src);
 
-void print_diagonal(int n);
+char *_strncat(char *dest, char *src, int n);
 
-void print_square(int size);
+char *_strncpy(char *dest, char *src, int n);
 
-void print_triangle(int size);
+int _strcmp(char *s1, char *s2);
 
-void print_number(int n);
+char *_memset(char *s, char b, unsigned int n);
 
+char *_memcpy(char *dest, char *src, unsigned int n);
 
+char *_strchr(char *s, char c);
+
+unsigned int _strspn(char *s, char *accept);
+
+char *_strpbrk(char *s, char *accept);
+
+char *_strstr(char *haystack, char *needle);
 
 #endif /* MAIN_H */
